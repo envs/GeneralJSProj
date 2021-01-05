4e5189be1e61a046f55bf655fa29be0fc9083931
envs <olaonipekun2001@yahoo.com>
2021-01-05 14:06:40 +0200

    Added routing function & pages.js with different pages components

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from "react";
+//import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import { Home, About, Events, Contact } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and Save to Reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+
+// return (
+//   <div className="App">
+//     <header className="App-header">
+//       <img src={logo} className="App-logo" alt="logo" />
+//       <p>
+//         Edit <code>src/App.js</code> and Save to Reload.
+//       </p>
+//       <a
+//         className="App-link"
+//         href="https://reactjs.org"
+//         target="_blank"
+//         rel="noopener noreferrer"
+//       >
+//         Learn React
+//       </a>
+//     </header>
+//   </div>
+// );
