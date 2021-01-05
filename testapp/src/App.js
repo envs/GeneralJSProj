4470b4a03684f107a12c5184d948f5f33d1dc46a
envs <olaonipekun2001@yahoo.com>
2021-01-05 14:40:57 +0200

    Linking sub-pages using Outlet

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -2,16 +2,21 @@ import React from "react";
 //import logo from './logo.svg';
 import './App.css';
 import {Routes, Route} from "react-router-dom";
-import { Home, About, Events, Contact } from "./pages";
+import { Home, About, Events, Contact, Error404, Services, CompanyHistory, Location } from "./pages";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<About />}>
+          <Route path="/services" element={<Services />} />
+          <Route path="/history" element={<CompanyHistory />} />
+          <Route path="/location" element={<Location />} />
+        </Route>
         <Route path="/events" element={<Events />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
   );
--- testapp/src/pages.js
+++ testapp/src/pages.js
@@ -1,9 +1,15 @@
 import React from "react";
+import {Link, useLocation, Outlet} from "react-router-dom";
 
 export function Home() {
     return (
         <div>
             <h1>[Company Website]</h1>
+            <nav>
+                <Link to="about">About</Link>
+                <Link to="events">Events</Link>
+                <Link to="contact">Contact</Link>
+            </nav>
         </div>
     );
 }
@@ -12,6 +18,7 @@ export function About() {
     return (
         <div>
             <h1>[About]</h1>
+            <Outlet />
         </div>
     );
 }
@@ -28,6 +35,43 @@ export function Contact() {
     return (
         <div>
             <h1>[Contact Us]</h1>
+        </div>
+    );
+}
+
+export function Error404() {
+    let location = useLocation();
+    return (
+        <div>
+            <center>
+            <h1>This page does not exist!!</h1>
+            <p>Resource not found at {location.pathname}</p>
+            </center>
+        </div>
+    );
+}
+
+// Subs for About Page
+export function Services() {
+    return (
+        <div>
+            <center><h2>Our Services</h2></center>
+        </div>
+    );
+}
+
+export function CompanyHistory() {
+    return (
+        <div>
+            <center><h2>Our Company History</h2></center>
+        </div>
+    );
+}
+
+export function Location() {
+    return (
+        <div>
+            <center><h2>Our Location(s)</h2></center>
         </div>
     );
 }
