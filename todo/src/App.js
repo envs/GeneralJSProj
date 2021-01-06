8e661b6c027d2aed28f482841f3d6ce1d26fc4a2
envs <olaonipekun2001@yahoo.com>
2021-01-06 14:48:06 +0200

    Webpack Dev Server Configuration and Compatibility

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { hot } from "react-hot-loader";
 import './App.css';
 
-const App = () => {
-    <div className="App">
+const App = () => (
+    <div>
         <h1>Hey!!</h1>
     </div>
-}
+);
 
-export default App;
+export default hot(App);
